08478071bb3ef38c0eefa3eed5dd169d66a8e919
Ran Millier <z861983448@gmail.com>
2022-05-02 18:02:35 +0800

    util/arena: 添加注释

--- util/arena.cc
+++ util/arena.cc
@@ -6,12 +6,13 @@
 
 namespace leveldb {
 
-static const int kBlockSize = 4096;
+static const int kBlockSize = 4096; // 块大小(KB)
 
 Arena::Arena()
     : alloc_ptr_(nullptr), alloc_bytes_remaining_(0), memory_usage_(0) {}
 
 Arena::~Arena() {
+  // 释放new[]分配的内存空间
   for (size_t i = 0; i < blocks_.size(); i++) {
     delete[] blocks_[i];
   }
@@ -21,11 +22,14 @@ char* Arena::AllocateFallback(size_t bytes) {
   if (bytes > kBlockSize / 4) {
     // Object is more than a quarter of our block size.  Allocate it separately
     // to avoid wasting too much space in leftover bytes.
+    // bytes大小超过kBlockSize值的1/4就单独分配1个大小为bytes的Block
+    // 避免在剩余的字节中浪费太多的内存空间
     char* result = AllocateNewBlock(bytes);
     return result;
   }
 
   // We waste the remaining space in the current block.
+  // 使用当前块的内存空间
   alloc_ptr_ = AllocateNewBlock(kBlockSize);
   alloc_bytes_remaining_ = kBlockSize;
 
@@ -36,12 +40,16 @@ char* Arena::AllocateFallback(size_t bytes) {
 }
 
 char* Arena::AllocateAligned(size_t bytes) {
+  /**
+   * 1. 至少以８字节大小进行对齐(通过计算机的地址位数得出)
+   * 2. 对齐字节数必须是2的n次方
+   */
   const int align = (sizeof(void*) > 8) ? sizeof(void*) : 8;
   static_assert((align & (align - 1)) == 0,
                 "Pointer size should be a power of 2");
-  size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align - 1);
-  size_t slop = (current_mod == 0 ? 0 : align - current_mod);
-  size_t needed = bytes + slop;
+  size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align - 1); // 取模运算
+  size_t slop = (current_mod == 0 ? 0 : align - current_mod); // 需要对齐的字节数量
+  size_t needed = bytes + slop; // 计算对齐后需要分配的内存空间
   char* result;
   if (needed <= alloc_bytes_remaining_) {
     result = alloc_ptr_ + slop;
@@ -49,6 +57,7 @@ char* Arena::AllocateAligned(size_t bytes) {
     alloc_bytes_remaining_ -= needed;
   } else {
     // AllocateFallback always returned aligned memory
+    // AllocateFallback总是返回对齐的内存(总是在新的内存块上操作, malloc默认对齐的)
     result = AllocateFallback(bytes);
   }
   assert((reinterpret_cast<uintptr_t>(result) & (align - 1)) == 0);
@@ -58,6 +67,7 @@ char* Arena::AllocateAligned(size_t bytes) {
 char* Arena::AllocateNewBlock(size_t block_bytes) {
   char* result = new char[block_bytes];
   blocks_.push_back(result);
+  // memory_usage_加上了vector中分配的char*内存空间
   memory_usage_.fetch_add(block_bytes + sizeof(char*),
                           std::memory_order_relaxed);
   return result;
--- util/arena.h
+++ util/arena.h
@@ -15,27 +15,39 @@ namespace leveldb {
 
 class Arena {
  public:
+  // 默认构造函数, 没有做任何特殊的操作
+  // 显示初始化成员变量为默认值
   Arena();
 
+  // 资源管理类, 不能进行拷贝和赋值
   Arena(const Arena&) = delete;
   Arena& operator=(const Arena&) = delete;
 
   ~Arena();
 
   // Return a pointer to a newly allocated memory block of "bytes" bytes.
+  // 返回指向新分配的"bytes"大小内存空间的指针地址, 即分配指定大小的内存空间
   char* Allocate(size_t bytes);
 
   // Allocate memory with the normal alignment guarantees provided by malloc.
+  // 使用和malloc一样的内存对齐方式分配内存
   char* AllocateAligned(size_t bytes);
 
   // Returns an estimate of the total memory usage of data allocated
   // by the arena.
+  // 返回当前已经分配的总内存
   size_t MemoryUsage() const {
     return memory_usage_.load(std::memory_order_relaxed);
   }
 
  private:
   char* AllocateFallback(size_t bytes);
+  /**
+   * 指定大小分配新的内存块Block存放到vector数组中
+   * 1. new[] block_bytes大小的内存空间
+   * 2. new的char*地址push_back到vector中
+   * 3. 更新memory_usage_成员变量
+   */
   char* AllocateNewBlock(size_t block_bytes);
 
   // Allocation state
@@ -43,12 +55,15 @@ class Arena {
   size_t alloc_bytes_remaining_;
 
   // Array of new[] allocated memory blocks
+  // vector存储使用new[]分配的内存空间, 即vector成员是内存块首地址
   std::vector<char*> blocks_;
 
   // Total memory usage of the arena.
-  //
+  // 总共已经使用了的内存空间, 原子变量
   // TODO(costan): This member is accessed via atomics, but the others are
   //               accessed without any locking. Is this OK?
+  // TODO: 只有memory_usage_使用的原子访问, 其他变量没有加锁, 其作用是什么？
+  // 只是保证MemoryUsage函数的线程安全性？
   std::atomic<size_t> memory_usage_;
 };
 
@@ -56,14 +71,14 @@ inline char* Arena::Allocate(size_t bytes) {
   // The semantics of what to return are a bit messy if we allow
   // 0-byte allocations, so we disallow them here (we don't need
   // them for our internal use).
-  assert(bytes > 0);
-  if (bytes <= alloc_bytes_remaining_) {
+  assert(bytes > 0); // 不提供分配0个内存的操作
+  if (bytes <= alloc_bytes_remaining_) {    // 当前内存块够用, 不用分配新的内存块
     char* result = alloc_ptr_;
     alloc_ptr_ += bytes;
     alloc_bytes_remaining_ -= bytes;
     return result;
   }
-  return AllocateFallback(bytes);
+  return AllocateFallback(bytes);   // 内存块空闲空间不够, 需要新分配
 }
 
 }  // namespace leveldb
